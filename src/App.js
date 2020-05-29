6aef5ec9ddcca4488d6f08c75818fa1555e5fb82
AtharvRedij <atharvaredij@gmail.com>
2020-05-29 20:46:47 +0530

    Fetched data from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    statesData: null,
+    districtsData: null,
+  };
+
+  fetchStateDetails = () => {
+    const url = "https://api.covid19india.org/data.json";
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((out) => {
+        this.setState({
+          statesData: out.statewise,
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+
+  fetchDistrictDetails = () => {
+    const url = "https://api.covid19india.org/state_district_wise.json";
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((out) => {
+        this.setState({
+          districtsData: out,
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+
+  componentDidMount() {
+    this.fetchStateDetails();
+    this.fetchDistrictDetails();
+  }
+
+  render() {
+    console.log(this.state);
+    return <h1>This is the homepage</h1>;
+  }
 }
 
 export default App;
